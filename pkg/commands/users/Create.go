5c398c7c017c6b7081a77acd254c823e815b2b89
qdnqn <adnn.selimovic@gmail.com>
2024-10-07 13:05:56 +0200

    Various improvements, Authentication, Multiple users with own certificates (#5)

---
+++ pkg/commands/users/Create.go
@@ -0,0 +1,17 @@
+package users
+
+import (
+	"fmt"
+	"github.com/simplecontainer/client/pkg/context"
+	"github.com/simplecontainer/client/pkg/network"
+)
+
+func Create(context *context.Context, username string, domain string, externalIP string) {
+	response := network.SendPost(context.Client, fmt.Sprintf("%s/api/v1/user/%s/%s/%s", context.ApiURL, username, domain, externalIP), nil)
+
+	fmt.Println(response.Explanation)
+
+	if response.Error {
+		fmt.Println(response.ErrorExplanation)
+	}
+}
