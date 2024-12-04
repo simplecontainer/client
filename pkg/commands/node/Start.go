a5fbadf540edcd9c89dfa2f6eec6e0d4c9f4791e
qdnqn <adnn.selimovic@gmail.com>
2024-12-04 21:24:17 +0100

    Minor improvements

--- pkg/commands/node/Start.go
+++ pkg/commands/node/Start.go
@@ -5,10 +5,13 @@ import (
 	"github.com/simplecontainer/client/pkg/manager"
 	"github.com/simplecontainer/client/pkg/network"
 	"github.com/spf13/viper"
+	"strconv"
 )
 
 func Start(mgr *manager.Manager) {
-	response := network.SendPost(mgr.Context.Client, fmt.Sprintf("%s/cluster/start", mgr.Context.ApiURL), map[string]any{
+	fmt.Println(fmt.Sprintf("%s/cluster/start/%s", mgr.Context.ApiURL, strconv.FormatBool(viper.GetBool("join"))))
+
+	response := network.SendPost(mgr.Context.Client, fmt.Sprintf("%s/cluster/start/%s", mgr.Context.ApiURL, strconv.FormatBool(viper.GetBool("join"))), map[string]any{
 		"cluster": viper.GetString("cluster"),
 		"url":     viper.GetString("url"),
 		"node":    viper.GetString("node"),
