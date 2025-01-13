2a5b7a5a6983359a2f8b9a7f312399bcc08dbf9b
qdnqn <adnn.selimovic@gmail.com>
2025-01-13 16:14:43 +0100

    Implement: Logs for containers streaming and reading

--- pkg/commands/objects/Logs.go
+++ pkg/commands/objects/Logs.go
@@ -24,7 +24,7 @@ func Logs() contracts.Command {
 		Functions: []func(*manager.Manager, []string){
 			func(mgr *manager.Manager, args []string) {
 				if len(os.Args) > 3 {
-					resp, err := network.Raw(mgr.Context.Client, fmt.Sprintf("%s/api/v1/logs/%s/%s/%s", mgr.Context.ApiURL, os.Args[3], os.Args[4], strconv.FormatBool(viper.GetBool("f"))), http.MethodGet, nil)
+					resp, err := network.Raw(mgr.Context.Client, fmt.Sprintf("%s/api/v1/logs/%s/%s/%s", mgr.Context.ApiURL, os.Args[2], os.Args[3], strconv.FormatBool(viper.GetBool("f"))), http.MethodGet, nil)
 
 					if err != nil {
 						fmt.Println(err)
@@ -37,7 +37,17 @@ func Logs() contracts.Command {
 					for {
 						bytes, err = resp.Body.Read(buff)
 
-						if bytes == 0 || err == io.EOF {
+						if err == io.EOF {
+							err = resp.Body.Close()
+
+							if err != nil {
+								fmt.Println(err)
+								os.Exit(1)
+							}
+							break
+						}
+
+						if bytes == 0 {
 							err = resp.Body.Close()
 
 							if err != nil {
@@ -45,7 +55,6 @@ func Logs() contracts.Command {
 								os.Exit(1)
 							}
 
-							fmt.Println("server closed connection")
 							break
 						}
 
--- pkg/startup/Startup.go
+++ pkg/startup/Startup.go
@@ -81,7 +81,7 @@ func ReadFlags(configObj *configuration.Configuration) {
 	flag.String("fcidr", "10.10.0.0/16", "Flannel overlay network CIDR")
 	flag.String("fiface", "", "Network interface for flannel to use, if ommited default gateway will be used")
 
-	flag.String("image", "simplecontainermanager/smr", "The smr image repo")
+	flag.String("image", "quay.io/simplecontainer/smr", "The smr image repo")
 	flag.String("tag", "latest", "The smr image tag")
 	flag.String("entrypoint", "/opt/smr/smr", "Entrypoint for the smr")
 	flag.String("args", "create smr --agent smr-agent", "args")
